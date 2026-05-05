618dd501ea7f0b102c86447e3bd57742daeeff67
Hahyun Cho <harrycho@kaist.ac.kr>
2026-05-06 03:27:55 +0900

    Building project structure

---
+++ Program.fs
@@ -0,0 +1,9 @@
+﻿/// KAIST CS.20200 Term Project
+
+[<EntryPoint>]
+let main _args =
+  printfn "Project Title: CLI 3-person Japanese Mahjong"
+  printfn "There is nothing here as of now,"
+  printfn "but more content will be added in due time..."
+  // TBA
+  0
