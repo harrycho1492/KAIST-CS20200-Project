3eedd9655b794fc01002838a6cdeeefddee43f98
Hahyun Cho <harrycho@kaist.ac.kr>
2026-05-06 10:59:05 +0900

    Building project structure

--- Mahjong.fs
+++ Mahjong.fs
@@ -1,6 +1,7 @@
 namespace TermProject
 
 type Mahjong (userPos) =
+  let status = GameStatus ()
   let players =
     match userPos with
     | 1 -> [| true; false; false |]
@@ -8,13 +9,14 @@ type Mahjong (userPos) =
     | 3 -> [| false; false; true |]
     | _ -> failwith "Fatal error"
     // TBA - Add further information in players list
-  // TBA - Game status indicator
 
   member __.Run () =
+    status.Init ()
     let printPlayer i = if (players[i]) then ("User") else ("Bot")
-    printfn "Dealer - East  : %s" (printPlayer 0)
-    printfn "Second - South : %s" (printPlayer 1)
-    printfn "Third  - West  : %s" (printPlayer 2)
-    printfn "The game isn't here as of now,"
-    printfn "but content will be added in due time..."
+    printfn "\nDealer - East  : %s"   (printPlayer 0)
+    printfn   "Second - South : %s"   (printPlayer 1)
+    printfn   "Third  - West  : %s\n" (printPlayer 2)
+    status.Display 0
+    printfn "The actual game isn't here as of now,"
+    printfn "but content will be added in due time...\n"
     // TBA
--- Program.fs
+++ Program.fs
@@ -1,16 +1,14 @@
 ﻿/// KAIST CS.20200 Term Project
 module TermProject.GameEntry
 
-open System
-
 let rec gameInit () =
   printfn "Select an option:"
   printfn "[1] Play as dealer / East"
   printfn "[2] Play as second / South"
   printfn "[3] Play as third  / West"
   printfn "[4] Exit"
-  Console.Write "> "
-  match Console.ReadLine () with
+  System.Console.Write "> "
+  match System.Console.ReadLine () with
   | "1" | "01" -> Mahjong(1).Run(); gameInit ()
   | "2" | "02" -> Mahjong(2).Run(); gameInit ()
   | "3" | "03" -> Mahjong(3).Run(); gameInit ()
@@ -19,5 +17,5 @@ let rec gameInit () =
 
 [<EntryPoint>]
 let main _args =
-  printfn "Project Title: CLI 3-person Japanese Mahjong"
+  printfn "Project Title: CLI 3-person Japanese Mahjong\n"
   gameInit ()
