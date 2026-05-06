1bb4ad5baf14b9191c79d2ba21e8b512d7547da9
Hahyun Cho <harrycho@kaist.ac.kr>
2026-05-07 03:11:51 +0900

    Building project structure

--- GameStatus.fs
+++ GameStatus.fs
@@ -7,75 +7,107 @@ type GameStatus () =
   // 11 ~ 13 : in hand of player 1 ~ 3
   // 14 ~ 16 : open in front of player 1 ~ 3
   // 17 ~ 19 : discarded in front of player 1 ~ 3
+  //      20 : unused
   let tiles = Array.create 108 0
   // for furiten
   // Player 1 :  0 ~ 26
   // Player 2 : 27 ~ 53
   // Player 3 : 54 ~ 80
   let hasDiscarded = Array.create 81 false
-  // for four quads abort
+  // TBA - temporary furiten memory
+  // for bonus tile display &  four quads abort
   let numQuads = 0
 
+  // for tile draws
+  let rand = System.Random ()
+  let rec drawTile () =
+    let idx = rand.Next (0, 108)
+    if (not (tiles[idx] = 0)) then (drawTile ()) else (idx)
+  
+  // internally used functions
+  let tileNotation idx =
+    match idx / 4 with
+    | 0  -> "1m"
+    | 1  -> "9m"
+    | 2  -> "1p"
+    | 3  -> "2p"
+    | 4  -> "3p"
+    | 5  -> "4p"
+    | 6  -> if (idx % 4 = 0) then ("5P") else ("5p") // Red tile bonus
+    | 7  -> "6p"
+    | 8  -> "7p"
+    | 9  -> "8p"
+    | 10 -> "9p"
+    | 11 -> "1s"
+    | 12 -> "2s"
+    | 13 -> "3s"
+    | 14 -> "4s"
+    | 15 -> if (idx % 4 = 0) then ("5S") else ("5s") // Red tile bonus
+    | 16 -> "6s"
+    | 17 -> "7s"
+    | 18 -> "8s"
+    | 19 -> "9s"
+    | 20 -> "1z"
+    | 21 -> "2z"
+    | 22 -> "3z"
+    | 23 -> "4z"
+    | 24 -> "5z"
+    | 25 -> "6z"
+    | 26 -> "7z"
+    | _  -> failwith "Fatal error"
+  let remainingTiles () =
+    Array.fold (fun x y -> if (y = 0) then (x + 1) else (x)) 0 tiles
+
   member __.Init () =
-    let rand = System.Random ()
-    let rec getIdx () =
-      let idx = rand.Next (0, 108)
-      if (not (tiles[idx] = 0)) then (getIdx ()) else (idx)
     // Getting bonus tiles
     for i in 1..10 do
-      tiles[getIdx ()] <- i
+      tiles[drawTile ()] <- i
+    // Getting unused tiles
+    for i in 1..5 do
+      tiles[drawTile ()] <- 20
     // Getting initial hand tiles
     for i in 1..39 do
-      tiles[getIdx ()] <- 11 + ((i - 1) % 3)
+      tiles[drawTile ()] <- 11 + ((i - 1) % 3)
+    ()
+  
+  member __.CanDrawTile () =
+    Array.exists (fun x -> x = 0) tiles
+
+  member __.DrawTile player =
+    if ((player > 3) || (player < 1)) then (failwith "Fatal error")
+    let drawnTile = drawTile ()
+    tiles[drawnTile] <- 10 + player
+    tileNotation drawnTile
+  
+  member __.DiscardTile idx player =
+    if ((player > 3) || (player < 1)) then (failwith "Fatal error")
+    if (not (tiles[idx] = 10 + player)) then (failwith "Invalid discard")
+    tiles[idx] <- 16 + player
     ()
 
   member __.ConstructInfo viewPoint =
-    // For bot algorithm building, TBA
+    // For data that bot algorithm will use, TBA
     ()
 
   member __.Display viewPoint =
-    let tileNotation n =
-      match n / 4 with
-      | 0  -> "1m"
-      | 1  -> "9m"
-      | 2  -> "1p"
-      | 3  -> "2p"
-      | 4  -> "3p"
-      | 5  -> "4p"
-      | 6  -> if (n % 4 = 0) then ("5P") else ("5p") // Red tile bonus
-      | 7  -> "6p"
-      | 8  -> "7p"
-      | 9  -> "8p"
-      | 10 -> "9p"
-      | 11 -> "1s"
-      | 12 -> "2s"
-      | 13 -> "3s"
-      | 14 -> "4s"
-      | 15 -> if (n % 4 = 0) then ("5S") else ("5s") // Red tile bonus
-      | 16 -> "6s"
-      | 17 -> "7s"
-      | 18 -> "8s"
-      | 19 -> "9s"
-      | 20 -> "1z"
-      | 21 -> "2z"
-      | 22 -> "3z"
-      | 23 -> "4z"
-      | 24 -> "5z"
-      | 25 -> "6z"
-      | 26 -> "7z"
-      | _  -> failwith "Fatal error"
     match viewPoint with
     | 0 -> // All-seeing observer perspective
       printfn "Bonus tiles"
-      let bonus = [| 1..10 |] |> Array.map (fun x -> tileNotation (Array.findIndex (fun y -> y = x) tiles))
-      printfn "%s | %s | %s | %s | %s" bonus[0] bonus[1] bonus[2] bonus[3] bonus[4]
-      printfn "%s | %s | %s | %s | %s" bonus[5] bonus[6] bonus[7] bonus[8] bonus[9]
+      let b1 = [ 1..5 ]  |> List.map (fun x -> tileNotation (Array.findIndex (fun y -> y = x) tiles))
+      let b2 = [ 6..10 ] |> List.map (fun x -> tileNotation (Array.findIndex (fun y -> y = x) tiles))
+      printfn "%s" (List.fold (fun x y -> x + " | " + y) b1.Head b1.Tail)
+      printfn "%s" (List.fold (fun x y -> x + " | " + y) b2.Head b2.Tail)
       printfn ""
       for i in 1..3 do
         printfn "In player %d's hand:" i
-        let ht = Array.map (fun (x, _) -> tileNotation x) (Array.filter (fun (x, y) -> y = (10 + i)) (Array.indexed tiles))
-        printfn "%s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s\n" ht[0] ht[1] ht[2] ht[3] ht[4] ht[5] ht[6] ht[7] ht[8] ht[9] ht[10] ht[11] ht[12]
+        let ht = Array.toList (
+          Array.map (fun (x, _) -> tileNotation x) (Array.filter (fun (x, y) -> y = (10 + i)) (Array.indexed tiles))
+        )
+        if (List.length ht > 1) then (
+          printfn "%s\n" (List.fold (fun x y -> x + " | " + y) ht.Head ht.Tail)
+        ) else (printfn "%s\n" ht.Head)
       // TBA - How to display open tiles & discarded tiles
+      printfn "%d tiles remaining in the stack\n" (remainingTiles ())
     | 1 -> failwith "Not implemented"
     | 2 -> failwith "Not implemented"
     | 3 -> failwith "Not implemented"
--- Mahjong.fs
+++ Mahjong.fs
@@ -2,21 +2,39 @@ namespace TermProject
 
 type Mahjong (userPos) =
   let status = GameStatus ()
-  let players =
-    match userPos with
-    | 1 -> [| true; false; false |]
-    | 2 -> [| false; true; false |]
-    | 3 -> [| false; false; true |]
-    | _ -> failwith "Fatal error"
+  let players = [| for i in 1..3 -> if (i = userPos) then (true) else (false) |]
     // TBA - Add further information in players list
 
   member __.Run () =
+    let rec mainLoop turn =
+      // draw tile
+      let drawnTile = status.DrawTile turn
+      printfn "Player %d has drawn: %s\n" turn drawnTile
+      (* if (players[turn - 1]) then (
+        // TBA - call for user interaction
+      ) else (
+        // TBA - call for bot interaction
+      ) *)
+      // TBA - reaction from other players
+      // TBA - check if there is no reaction
+      // check for no winning hand abort & call next player
+      if (status.CanDrawTile ()) then (
+        mainLoop ((turn % 3) + 1)
+      ) else (
+        ()
+      )
+    
+    let printPlayer i = if (players[i - 1]) then ("User") else ("Bot")
+    printfn "\nDealer - East  : %s"   (printPlayer 1)
+    printfn   "Second - South : %s"   (printPlayer 2)
+    printfn   "Third  - West  : %s\n" (printPlayer 3)
+
     status.Init ()
-    let printPlayer i = if (players[i]) then ("User") else ("Bot")
-    printfn "\nDealer - East  : %s"   (printPlayer 0)
-    printfn   "Second - South : %s"   (printPlayer 1)
-    printfn   "Third  - West  : %s\n" (printPlayer 2)
-    status.Display 0
+    status.Display 0   // for debugging
+
+    mainLoop 1
+    status.Display 0   // for debugging
+
     printfn "The actual game isn't here as of now,"
     printfn "but content will be added in due time...\n"
     // TBA
